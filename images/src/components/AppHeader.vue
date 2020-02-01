c8ca5d5722ba4de1aaf7357e32038e74f3ad6c67
Mark Faulkner <markfaulk350@gmail.com>
2020-02-01 11:03:15 -0800

    added persistence to login state with localstorage

--- images/src/components/AppHeader.vue
+++ images/src/components/AppHeader.vue
@@ -2,16 +2,20 @@
   <div class="ui secondary pointing menu">
     <a href="/" class="active item">Image Storage</a>
     <div class="right menu">
-      <a href="#" class="ui item" @click="login">Login</a>
+      <div v-if="isLoggedIn">
+        
+      </div>
+      <a v-else href="#" class="ui item" @click="login">Login</a>
     </div>
   </div>
 </template>
 
 <script>
-import { mapActions } from "vuex"; // Import a helper from vuex. mapActions is a function that will automagically connect different actions that we have created inside of our modules to a component
+import { mapActions, mapGetters } from "vuex"; // Import a helper from vuex. mapActions is a function that will automagically connect different actions that we have created inside of our modules to a component
 
 export default {
   name: "AppHeader",
+  computed: mapGetters(["isLoggedIn"]),
   methods: mapActions(["login"])
 };
 </script>
