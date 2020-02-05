41c91e81a073f35fa811438363f4cb91a9f0ae8e
Mark Faulkner <markfaulk350@gmail.com>
2020-02-04 20:19:58 -0800

    added ablity to add todo

--- traversy-todo/src/store/modules/todos.js
+++ traversy-todo/src/store/modules/todos.js
@@ -12,11 +12,19 @@ const actions = {
     async fetchTodos({ commit }) {  // We do not call mutations directly, we call them with commit.
         const response = await axios.get(`https://jsonplaceholder.typicode.com/todos`)
         commit('setTodos', response.data);  // We use commit to call mutations and pass data
+    },
+    async addTodo({ commit }, title) {
+        const response = await axios.post('https://jsonplaceholder.typicode.com/todos', {
+            title,
+            completed: false
+        })
+        commit('newTodo', response.data);
     }
 };
 
 const mutations = {
-    setTodos: (state, todos) => state.todos = todos 
+    setTodos: (state, todos) => (state.todos = todos),
+    newTodo: (state, todo) => state.todos.unshift(todo) 
 };
 
 export default {
